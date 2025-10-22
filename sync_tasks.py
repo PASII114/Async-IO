1c7d956dd5beff0a5de0dd6d980f1b886afc1404
PASII114 <devRushmika@gmail.com>
2025-10-22 15:06:49 +0530

    hi

---
+++ sync_tasks.py
@@ -0,0 +1,28 @@
+import time
+from concurrent.futures.thread import ThreadPoolExecutor
+from threading import Thread
+
+
+def execute_sync_task(task_name: str, duration: int):
+    print(f"Starting task - {task_name}")
+    time.sleep(duration)
+    print(f"Ending task - {task_name}")
+
+
+def main():
+    thread1 = Thread(target=execute_sync_task, args=("Task 1", 5))
+    thread2 = Thread(target=execute_sync_task, args=("Task 2", 2))
+    thread3 = Thread(target=execute_sync_task, args=("Task 3", 3))
+
+    thread1.start()
+    thread2.start()
+    thread3.start()
+
+
+def main_threadpool():
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        executor.submit(execute_sync_task, "Task 1", 5)
+        executor.submit(execute_sync_task, "Task 2", 2)
+        executor.submit(execute_sync_task, "Task 3", 3)
+
+main_threadpool()
