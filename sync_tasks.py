4274e18e9ec489ea82c03ee425cce1e8a5794b5e
PASII114 <devRushmika@gmail.com>
2025-10-22 16:01:09 +0530

    async

--- sync_tasks.py
+++ sync_tasks.py
@@ -25,4 +25,21 @@ def main_threadpool():
         executor.submit(execute_sync_task, "Task 2", 2)
         executor.submit(execute_sync_task, "Task 3", 3)
 
-main_threadpool()
+# main_threadpool()
+
+import asyncio
+
+async def execute_async_task(task_name: str, duration: int): #coroutine function
+    print(f"Starting task - {task_name}")
+    # time.sleep(duration)
+    print(f"Ending task - {task_name}")
+
+
+async def async_main():
+    await asyncio.gather(
+        execute_async_task("Task 1", 5),
+        execute_async_task("Task 2", 3),
+        execute_async_task("Task 3", 5)
+    )
+
+asyncio.run(async_main()) #initiating our event loop
